eff4124aa804b6c4e44ba89c8b977b05a2f045cd
sgeeta <sgeeta@sgeeta.pkrmsezdc.com>
2016-09-13 13:52:51 +0530

    Added New pojo class

---
+++ GitWebTest/src/HelloPojo.java
@@ -0,0 +1,18 @@
+
+public class HelloPojo {
+String name;
+int add;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getAdd() {
+	return add;
+}
+public void setAdd(int add) {
+	this.add = add;
+}
+
+}
